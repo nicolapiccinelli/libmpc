5bf6d958e8a8eec8f0f699f70ab3910f8835b3ef
Nicola Piccinelli <nicola.piccinelli@univr.it>
2022-06-15 13:47:28 +0000

    Get linear fix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # Changelog
 
+## [0.0.8.1] - 2022-06-15
+
+### Changed
+- Catch2 dependency updated to version 3
+
+### Fixed
+- Improved performances while building the optimization problem in LOptimizer
+
 ## [0.0.8] - 2022-03-02
 ### Added
 - Changelog added :)
--- include/mpc/Types.hpp
+++ include/mpc/Types.hpp
@@ -16,7 +16,8 @@
 
 #undef eigen_assert
 #define eigen_assert(x)                              \
-    if (!(x)) {                                      \
+    if (!(x))                                        \
+    {                                                \
         std::cout << boost::stacktrace::stacktrace() \
                   << std::endl                       \
                   << std::endl;                      \
@@ -28,107 +29,114 @@
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
 
-namespace mpc {
-
-template <
-    int M = Eigen::Dynamic,
-    int N = Eigen::Dynamic>
-using mat = Eigen::Matrix<double, M, N>;
-
-using smat = Eigen::SparseMatrix<double, Eigen::ColMajor>;
-
-template <
-    int N = Eigen::Dynamic>
-using cvec = Eigen::Matrix<double, N, 1>;
-
-template <
-    int N = Eigen::Dynamic>
-using rvec = Eigen::Matrix<double, 1, N>;
-
-/**
- * @brief Shared optimizer parameters
- */
-struct Parameters {
-protected:
-    Parameters() = default;
-    virtual ~Parameters() = default;
-
-public:
-    int maximum_iteration = 100;
-};
-
-/**
- * @brief Non-linear optimizer parameters
- */
-struct NLParameters : Parameters {
-    NLParameters() = default;
-    
-    double relative_ftol = 1e-10;
-    double relative_xtol = 1e-10;
-    bool hard_constraints = true;
-};
-
-/**
- * @brief Linear optimizer parameters
- */
-struct LParameters : Parameters {
-    LParameters() = default;
-
-    double alpha = 1.6;
-    double rho = 1e-6;
-   
-    double eps_rel = 1e-4;
-    double eps_abs = 1e-4;   
-    double eps_prim_inf = 1e-3;
-    double eps_dual_inf = 1e-3;
-    
-    bool verbose = false;
-    bool adaptive_rho = true;
-    bool polish = true;
-};
-
-/**
- * @brief Optimization control input result
- * 
- * @tparam Tnu dimension of the input space
- */
-template <int Tnu = Eigen::Dynamic>
-struct Result {
-    Result()
-        : retcode(0)
-        , cost(0)
-    {
-        cmd.setZero();
-    }
-
-    int retcode;
-    double cost;
-    cvec<Tnu> cmd;
-};
-
-enum constraints_type {
-    INEQ,
-    EQ,
-    UINEQ,
-    UEQ
-};
-
-/**
- * @brief Utility to get the dimension based on the input flag
- * 
- * @param n value
- * @param c flag
- * @return constexpr int input dimension or -1
- */
-inline constexpr int makeDim(const int n, bool c)
+namespace mpc
 {
-    if (c) {
-        return n;
-    } else {
-        return Eigen::Dynamic;
-    }
-}
 
-constexpr double inf = std::numeric_limits<double>::infinity();
+    template <
+        int M = Eigen::Dynamic,
+        int N = Eigen::Dynamic>
+    using mat = Eigen::Matrix<double, M, N>;
+
+    using smat = Eigen::SparseMatrix<double, Eigen::ColMajor>;
+
+    template <
+        int N = Eigen::Dynamic>
+    using cvec = Eigen::Matrix<double, N, 1>;
+
+    template <
+        int N = Eigen::Dynamic>
+    using rvec = Eigen::Matrix<double, 1, N>;
+
+    /**
+     * @brief Shared optimizer parameters
+     */
+    struct Parameters
+    {
+    protected:
+        Parameters() = default;
+        virtual ~Parameters() = default;
+
+    public:
+        int maximum_iteration = 100;
+    };
+
+    /**
+     * @brief Non-linear optimizer parameters
+     */
+    struct NLParameters : Parameters
+    {
+        NLParameters() = default;
+
+        double relative_ftol = 1e-10;
+        double relative_xtol = 1e-10;
+        bool hard_constraints = true;
+    };
+
+    /**
+     * @brief Linear optimizer parameters
+     */
+    struct LParameters : Parameters
+    {
+        LParameters() = default;
+
+        double alpha = 1.6;
+        double rho = 1e-6;
+
+        double eps_rel = 1e-4;
+        double eps_abs = 1e-4;
+        double eps_prim_inf = 1e-3;
+        double eps_dual_inf = 1e-3;
+
+        bool verbose = false;
+        bool adaptive_rho = true;
+        bool polish = true;
+    };
+
+    /**
+     * @brief Optimization control input result
+     *
+     * @tparam Tnu dimension of the input space
+     */
+    template <int Tnu = Eigen::Dynamic>
+    struct Result
+    {
+        Result(): retcode(0), cost(0)
+        {
+            cmd.setZero();
+        }
+
+        int retcode;
+        double cost;
+        cvec<Tnu> cmd;
+    };
+
+    enum constraints_type
+    {
+        INEQ,
+        EQ,
+        UINEQ,
+        UEQ
+    };
+
+    /**
+     * @brief Utility to get the dimension based on the input flag
+     *
+     * @param n value
+     * @param c flag
+     * @return constexpr int input dimension or -1
+     */
+    inline constexpr int makeDim(const int n, bool c)
+    {
+        if (c)
+        {
+            return n;
+        }
+        else
+        {
+            return Eigen::Dynamic;
+        }
+    }
+
+    constexpr double inf = std::numeric_limits<double>::infinity();
 
 } // namespace mpc
